688ecc727e105006b9f1e861e627490e19cb1cb0
boardguy1024 <boardguy1024@gmail.com>
2021-09-20 18:40:36 +0900

    Initial Commit

---
+++ SwiftUI_CompositionalLayout/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_CompositionalLayout
+//
+//  Created by park kyung seok on 2021/09/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_CompositionalLayout/SwiftUI_CompositionalLayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CompositionalLayoutApp.swift
+//  SwiftUI_CompositionalLayout
+//
+//  Created by park kyung seok on 2021/09/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CompositionalLayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
